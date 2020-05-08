785b54fa6ad268106a4128a83c34707935da0fd8
samedguzeltas <samedguzeltas@gmail.com>
2020-05-08 18:51:36 +0100

    add Spring.java

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -1,4 +1,17 @@
 package tests;
 
 public class Login {
+
+    public static void main(String[] args) {
+
+        System.out.println("Samed");
+
+//wrote code here
+
+
+
+
+
+    }
+
 }
---
+++ src/tests/Spring.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class Spring {
+
+    public static void main(String[] args) {
+
+        System.out.println("welcome spring");
+    }
+}
